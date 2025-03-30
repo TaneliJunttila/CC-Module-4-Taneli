7ac09e9aa668e108cd0138fda06de060d8b73429
Taneli Junttila <152056764+TaneliJunttila@users.noreply.github.com>
2025-03-30 15:08:32 +0300

    Test webhook 3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return "" \
     "<h1>Hello World!</h1>" \
-    "<h3>Version: 3</h3>"
+    "<h3>Version: 4</h3>"
 
 if __name__ == '__main__':
      app.run(host="0.0.0.0", port="8080", debug=False)
